7f08acb2019d6e6b530d25da21c12ee94bce1fa2
prettyblueberry <prettyblueberry52702@gmail.com>
2023-03-02 11:12:49 +0900

    server install body-parser

--- server/index.js
+++ server/index.js
@@ -4,12 +4,20 @@ import path from "path";
 import "./src/helpers/global.helper.js";
 import apiRouter from "./src/router/index.js";
 import cors from 'cors';
+import bodyParser from 'body-parser'
 
 const port = 3001;
 const app = express()
 
 app.use(morgan("dev"));
 app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //api router
 app.use('/api', apiRouter);
 
