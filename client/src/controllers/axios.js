1d6c3d84d55aba63c8ef550e58185cbcd81bf291
prettyblueberry <prettyblueberry52702@gmail.com>
2023-03-05 22:56:27 +0900

    client create .env for api url

--- client/src/controllers/axios.js
+++ client/src/controllers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api/',
+    baseURL: (process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "") +'/api/',
 });
 api.interceptors.response.use((response) => response, (error) => {
     // whatever you want to do with the error
