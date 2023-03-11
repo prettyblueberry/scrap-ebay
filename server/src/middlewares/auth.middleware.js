b222a02d15860f589f9d2ffd52083d78369fb48c
prettyblueberry <prettyblueberry52702@gmail.com>
2023-03-11 09:49:34 +0900

    server tracking pending bug

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwtHelper from "../helpers/jwt.helper.js";
 import HS from "http-status-codes";
 export  function authMiddleware(req, res, next){
-    console.log("!@#tracker", "authMiddleware");
+    console.log("!@#tracker", "authMiddleware", req.login, req.jsession);
     if(req.login) return next();
     res.sendStatus(HS.UNAUTHORIZED);
 }
@@ -9,8 +9,10 @@ export  function authMiddleware(req, res, next){
 export function jwtSession(req, res, next){
     let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
     const token = req.header(tokenHeaderKey);
+    console.log("!@#tracker", "token", tokenHeaderKey, token);
     if(token){
         const verified = jwtHelper.verifyToken(token);
+        console.log("!@#tracker", "token verified", verified);
         if(verified){
             req.login = true;
             req.jsession = { ...verified };
